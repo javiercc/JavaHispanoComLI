ab2fa55b3446e5803648840c79226395bbb7b8d5
Samsung <Samsung@Samsung-PC>
2014-03-29 20:33:55 +0100

    Primer Commit

---
+++ JavaHispanoComLI/src/main/java/es/javahispano/App.java
@@ -0,0 +1,13 @@
+package es.javahispano;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
